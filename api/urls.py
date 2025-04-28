0944544034b65ec44cf74f9863a1a75836e16f47
Vikash <vikastsr@gmail.com>
2025-04-28 12:53:31 +0530

    2nd changes

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import welcome
+from .views import welcome, movie_list
+# movie_list
 
 urlpatterns = [
     path('welcome/', welcome),
+    path('movies/', movie_list),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,15 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Movie
+from .serializers import MovieSerializer
+
 
 def welcome(request):
     return JsonResponse({"message": "Welcome to the Movie Review API!"})
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
